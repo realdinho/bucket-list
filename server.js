dea4d02263dc8dfd5d81b9f109bf2b1fb2758934
Realdo Dias <realdo@omesti.com>
2020-10-26 14:23:40 +0800

    update configurations

--- server.js
+++ server.js
@@ -1,7 +1,30 @@
-const express = require('express');
-const app = express();
-const PORT = 3000;
+const express = require('express')
+const app = express()
+const dotenv = require('dotenv')
+const mongoose = require('mongoose')
+const cors = require('cors')
+const morgan = require('morgan')
+const bodyParser = require('body-parser')
 
-app.get('/', (req, res) => res.send('Hello world!'));
+// get environment variables
+dotenv.config({ path: './config.env' })
 
-app.listen(PORT, () => console.log(`App listening at http://localhost:${PORT}`));
+// proxies
+app.use(cors())
+app.use(morgan('tiny'))
+app.use(bodyParser.json())
+
+// connect to database
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+  })
+  .then(() => console.log('MongoDB database connected...'))
+  .catch((err) => console.log(err))
+
+app.get('/', (req, res) => res.send('Hello world!'))
+
+app.listen(process.env.PORT, () => console.log(`App listening at http://localhost:${process.env.PORT}`))
