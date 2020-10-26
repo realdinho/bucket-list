57bf808724e2bda9cea6aea01fb0f84c15025266
Realdo Dias <realdo@omesti.com>
2020-10-26 15:42:32 +0800

    add CDNs for style & fonts and implement adding item

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,28 +1,53 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="field has-addons">
+      <div class="control is-expanded">
+        <input type="text" class="input" v-model="description" placeholder="Buy groceries...">
+      </div>
+      <div class="control">
+        <a class="button is-info" @click="addItem" :disabled="!description">Add</a>
+      </div>
+    </div>
+    <div class="notification" v-for="(item, i) in items" :key="item._id">
+      <p>
+        <span class="tag is-primary">{{ i + 1 }}</span>
+        {{ item.description }}
+      </p>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios';
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      items: [],
+      description: '',
+    }
+  },
+  async mounted() {
+    const response = await axios.get('api/bucketListItems/');
+    this.items = response.data;
+  },
+  methods: {
+    async addItem() {
+      const response = await axios.post('api/bucketListItems/', {
+        description: this.description
+      })
+      this.items.push(response.data)
+      this.description = ''
+    }
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  margin: auto;
+  margin-top: 3rem;
+  max-width: 700px;
 }
 </style>
